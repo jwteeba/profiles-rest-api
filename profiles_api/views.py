dd57db1ec24b920b996471392762acde2888b9b8
Jah-Wilson Teeba <teebawilson91@gmail.com>
2020-06-21 20:34:17 -0400

    Created Serializers object

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from profiles_api import models
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+from rest_framework import viewsets
 
-# Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()

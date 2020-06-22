22a05787ee3edfcc2bd429368811b926094b968e
Jah-Wilson Teeba <teebawilson91@gmail.com>
2020-06-21 21:04:42 -0400

    Added search and filter for user profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,7 @@ from profiles_api import serializers
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
+from rest_framework import filters
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
@@ -14,3 +15,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)

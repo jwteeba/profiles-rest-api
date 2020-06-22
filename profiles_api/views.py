e5ad8cdac97f2e0ab934dfdcba3654bb8d4d8582
Jah-Wilson Teeba <teebawilson91@gmail.com>
2020-06-21 20:57:45 -0400

    Added profile viewset with permission

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,9 +4,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating, creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
